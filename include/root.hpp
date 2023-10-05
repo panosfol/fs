1ccc1aa8ff825b9d7191593dc67081295c76f85d
Panagiotis Foliadis <pfoliadis@hotmail.com>
2023-10-05 04:31:12 +0300

    Add basic methods for `root` class
    Signed-off-by: Panagiotis Foliadis <pfoliadis@hotmail.com>

--- include/root.hpp
+++ include/root.hpp
@@ -1,11 +1,29 @@
 #ifndef ROOT_H_NAME
 #define ROOT_H_NAME
 
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
 #include "dir.hpp"
 
-class Root : public Directory {
+class Root {
+protected:
+    std::unordered_map<std::string, File *> contents;
 public:
+  Root();
+
+  void main_loop();
+
   std::string file_search();
   std::string file_delete(std::string);
+  std::vector<std::string> parse_string();
+
+  std::vector<std::string> input;
+
+  std::string current_path;
+  std::string user_input;
+
+  Directory *current_dir;
 };
 #endif
